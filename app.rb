ab82d86b7138440fead54c0b7ce8f9d5d09bcd1b
Charlie Cook <turingbombe@gmail.com>
2016-08-12 19:05:14 -0400

    Puppy forms

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do 
+    erb :index
+  end
+
+  post '/dog' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+
+  get '/new' do 
+    erb :create_puppy
+  end
 end
